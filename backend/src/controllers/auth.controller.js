ecf9a1ae89f4b59a3ac21cff3e0b86f53711ef79
Faizal Shaikh <webworksbyfaiz@gmailcom>
2024-11-22 03:30:56 +0530

    Implement user signup functionality with JWT token generation
    - Validate required fields and ensure password length
    - Use bcrypt for password hashing
    - Create new user in the database and generate JWT token
    - Set secure HTTP-only cookie for authentication
    - Handle errors with appropriate responses

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,58 @@
-export const signup = (req, res) => {
-  res.send("signup page");
+import bcrypt from "bcryptjs";
+import User from "../models/user.model.js";
+import { generateToken } from "../lib/utils.js";
+
+export const signup = async (req, res) => {
+  const { email, fullName, password } = req.body;
+  try {
+    // Check if any required field is missing
+    if (!email || !fullName || !password) {
+      return res.status(400).json({ message: "Please fill in all fields" });
+    }
+
+    // Validate password length
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be atleast 6 characters long" });
+    }
+
+    // Check if the user already exists in the database
+    const user = await User.findOne({ email });
+    if (user) return res.status(400).json({ message: "Email already exists" });
+
+    // Hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create a newUser
+    const newUser = await User({
+      email,
+      fullName,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      // Generate a JWT token for the user
+      generateToken(newUser._id, res);
+
+      // Save the new user to the database
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePicture: newUser.profilePicture,
+      });
+    } else {
+      // Respond with an error if the user data is invalid
+      res.status(400).json({ message: "Invalid user data" });
+    }
+  } catch (error) {
+    console.log("Error in signup controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 
 export const login = (req, res) => {
